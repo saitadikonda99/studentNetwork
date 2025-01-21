36108ea798c9c641ad2a38952a3bf20d97517ccd
saitadikonda99 <saitadikonda.tech@gmail.com>
2025-01-21 23:35:17 +0530

    initial setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './page.css';
+
+export default function Home() {
+  return (
+      <div className="HomeComponent">
+        <div className="HomeComponent-in">
+          <p>Welcome to the official webpage of StudentNetwork</p>
+        </div>
+      </div>
+  );
+}
