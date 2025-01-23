5cda33574c27a132807a4264aca7b637abf6d3e6
saitadikonda99 <saitadikonda.tech@gmail.com>
2025-01-23 22:06:56 +0530

    login page re-designed and updated

--- my-app/src/app/(pages)/auth/login/page.tsx
+++ my-app/src/app/(pages)/auth/login/page.tsx
@@ -8,10 +8,11 @@ import './page.css';
 
 // import start from here
 import { FaRegCopyright } from "react-icons/fa";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 export default function Login() {
-
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value);
@@ -21,21 +22,24 @@ export default function Login() {
         console.log('Login button clicked');
     }
 
-
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
 
   return (
       <div className="LoginComponent">
         <div className="LoginComponent-in">
 
-            <div className="login-one">
+            <div className="Login-one">
+            <div className="login-one-one">
                 <h1> <span>{"["}</span> StudentNetwork <span>{"]"}</span> </h1>
             </div>
 
-            <div className="login-two">
+            <div className="login-one-two">
                 <p>Welcome  back! Please login to your account.</p>
             </div>
 
-            <div className="login-three">
+            <div className="login-one-three">
                 <input
                     type="text"
                     placeholder='Enter the email or username'
@@ -44,24 +48,33 @@ export default function Login() {
                 />
             </div>
 
-            <div className="login-four">
-                <input
-                    type="password"
-                    placeholder='Enter the password'
-                    className='LoginComponent-input'
-                    onChange={() => handleChange}
-                />
+            <div className="login-one-four">
+                <div className="password-input-container">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        placeholder='Enter the password'
+                        className='LoginComponent-input'
+                        onChange={() => handleChange}
+                    />
+                    <button 
+                        className="password-toggle-btn"
+                        onClick={togglePasswordVisibility}
+                        type="button"
+                    >
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </button>
+                </div>
             </div>
 
-            <div className="login-five">
+            <div className="login-one-five">
                 <Link href='/auth/forgot-password'>
                     Forgot Password?
                 </Link>
             </div>
 
 
-            <div className="login-six">
-                <div className="login-six-in">
+            <div className="login-one-six">
+                <div className="login-one-six-in">
                     <button
                         className='LoginComponent-button'
                         onClick={handleLogin}
@@ -77,17 +90,18 @@ export default function Login() {
                     </Link>
                 </div>
             </div>
+            </div>
 
-            <div className="login-seven">
-                <div className="login-seven-in">
-                    <div className="login-seven-one">
+            <div className="login-two">
+                <div className="login-two-in">
+                    <div className="login-two-one">
                         <Link href='/'>about</Link>
                         <Link href='/'>contact</Link>
                         <Link href='/'>faq</Link>
                         <Link href='/'>terms</Link>
                         <Link href='/'>privacy</Link>
                     </div>
-                    <div className="login-seven-two">
+                    <div className="login-two-two">
                         <p>a Tadikonda Sai Manikanta production</p>
                         <p>StudentNetwork <FaRegCopyright /> 2025 </p>
                     </div>
