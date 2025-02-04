9e165de289a90a6e60fc76caa3e61e35bc1e4da0
saitadikonda99 <saitadikonda.tech@gmail.com>
2025-02-04 23:51:13 +0530

    initial setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./page.css";
+
+export default function Home() {
+  return (
+    <div className="HomeComponent">
+      <div className="HomeComponent-in">
+        <h1>S</h1>
+      </div>
+    </div>
+  );
+}
